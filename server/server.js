98e7dce55049c4bedb108be5d23fbbaed2f71b94
Samantha Nthambi Mbila <128149228+ykitsama@users.noreply.github.com>
2025-09-03 19:42:19 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const poemsRouter = require('./routes/poems');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/poetry_blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use('/api/poems', poemsRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
